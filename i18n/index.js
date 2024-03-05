f63370583aaac3c93949b8ec8d31b19f5ee1083c
VL3BOX <admin@vl3box.com>
2024-03-05 11:51:07 +0700

    i18n: update func

--- i18n/index.js
+++ i18n/index.js
@@ -1,26 +1,22 @@
-import Vue from "vue";
 import VueI18n from "vue-i18n";
 import { $cms } from "../js/https";
 import moment from "moment";
 import dayjs from "dayjs";
 import { zhcnDateTimeFormat } from "./datetime";
 
-Vue.use(VueI18n);
 
 // default language that is preloaded
 const loadedLanguages = ["zh-CN"];
+const currentLang = sessionStorage.getItem("lang") || "zh-CN";
+const currentMessages = JSON.parse(sessionStorage.getItem("locale") || "{}");
 
-const translationWarn = ~~sessionStorage.getItem("translationWarn");
 const i18n = new VueI18n({
-  locale: "zh-CN",
+  locale: currentLang,
   fallbackLocale: "zh-CN",
   formatFallbackMessages: true,
-  messages: {},
-  silentTranslationWarn: !translationWarn, // process.env.NODE_ENV === "production",
+  messages: currentMessages,
+  silentTranslationWarn: process.env.NODE_ENV === "production",
 });
-
-const currentLang = sessionStorage.getItem("lang") || "zh-CN";
-console.log("currentLang", currentLang);
 
 export function changeLocale(lang) {
   if (currentLang === "zh-CN") {
@@ -29,34 +25,37 @@ export function changeLocale(lang) {
     dayjs.locale("zh-cn", zhcnDateTimeFormat);
   } else {
     // momment or dayjs locale
-    moment.locale("vi");
-    dayjs.locale("vi");
+    moment.locale(lang);
+    dayjs.locale(lang);
   }
   sessionStorage.setItem("lang", lang);
   loadLanguageAsync(lang);
-  // window.location.reload();
+  window.location.reload();
 }
 
-function setI18nLanguage(lang) {
+export function setLocale(lang) {
   i18n.locale = lang;
   // axios.defaults.headers.common['Accept-Language'] = lang
   document.querySelector("html").setAttribute("lang", lang);
   return lang;
 }
 
+export function getLocale() {
+  return i18n.locale;
+}
+
 export function loadLanguageAsync(lang) {
   // If the same language
   if (i18n.locale === lang) {
-    return Promise.resolve(setI18nLanguage(lang));
+    return Promise.resolve(setLocale(lang));
   }
 
   // If the language was already loaded
   if (loadedLanguages.includes(lang)) {
-    return Promise.resolve(setI18nLanguage(lang));
+    return Promise.resolve(setLocale(lang));
   }
 
   async function importOffline(lang) {
-    console.log("OFFLINE", lang);
     const messages = await import(
       /* webpackChunkName: "[request]" */ `./locales/${lang}.json`
     );
@@ -64,12 +63,10 @@ export function loadLanguageAsync(lang) {
     sessionStorage.setItem("locale", JSON.stringify(messages.default));
     i18n.setLocaleMessage(lang, messages.default);
     loadedLanguages.push(lang);
-    return setI18nLanguage(lang);
+    return setLocale(lang);
   }
 
   async function importOnline(lang) {
-    console.log("ONLINE", lang);
-
     return $cms()
       .get(`/locales/${lang}.json`)
       .then((res) => {
@@ -77,7 +74,7 @@ export function loadLanguageAsync(lang) {
         loadedLanguages.push(lang);
 
         i18n.setLocaleMessage(lang, res.data);
-        return setI18nLanguage(lang);
+        return setLocale(lang);
       })
       .catch(() => {
         return importOffline(lang);
@@ -88,7 +85,7 @@ export function loadLanguageAsync(lang) {
   if (langData) {
     loadedLanguages.push(lang);
     i18n.setLocaleMessage(lang, JSON.parse(langData));
-    return setI18nLanguage(lang);
+    return setLocale(lang);
   }
   return process.env.OFFLINE ? importOffline(lang) : importOnline(lang);
 }
